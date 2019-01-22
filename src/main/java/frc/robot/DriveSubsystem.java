5b7a55e086fa891d7090ece3ee4f74dd694425f9
Jake Charron <jcharron0306@gmail.com>
2019-01-22 17:27:46 -0500

    (DAVID) Added limelight tracking feature for fun and to test limelight code

--- src/main/java/frc/robot/DriveSubsystem.java
+++ src/main/java/frc/robot/DriveSubsystem.java
@@ -6,6 +6,9 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -34,6 +37,13 @@ public class DriveSubsystem extends Subsystem {
 
   ControllerSubsytem control = new ControllerSubsytem();
 
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry ty = table.getEntry("ty");
+  NetworkTableEntry ta = table.getEntry("ta");
+  NetworkTableEntry tv = table.getEntry("tv");
+
+
   // SpeedControllerGroup leftGroup = new SpeedControllerGroup(leftFrontVictor, leftRearVictor);
 	// SpeedControllerGroup rightGroup = new SpeedControllerGroup(rightFrontVictor, rightRearVictor);
   // DifferentialDrive difDrive = new DifferentialDrive(leftGroup, rightGroup);
@@ -61,7 +71,49 @@ public class DriveSubsystem extends Subsystem {
 		turn = OI.controller.getX() > 0 ? OI.controller.getX()*1 : OI.controller.getX()*1;
 		throttle = control.getTrigerThrottle(OI.controller.getTwist(), OI.controller.getThrottle());
 		difDrive.arcadeDrive(throttle,turn);
-	}
+  }
+  
+  public void followTarget() {
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+    double hasTargets = tv.getDouble(0.0);
+    if(hasTargets == 1.0) {
+      
+      // System.out.println("X: " + x);
+      // go left / right until x <= 1 or x >= -1
+      // if x > 1 turn left else turn right slowly
+      // then go forward while y > 1 (to be safe)
+      if(x <= 2 && x >= -2) {
+        System.out.println("LESS THAN 2 AND GREATER THAN 2");
+        if(y < -7) {
+          difDrive.arcadeDrive(0, 0);
+        } else {
+          difDrive.arcadeDrive(0.5, 0);
+        }
+      } else {
+        
+        if (x > 1) {
+          // turn left
+          System.out.println("LEFT");
+
+          difDrive.arcadeDrive(0.2, 0.5);
+          
+        } else {
+          // turn right
+          System.out.println("RIGHT");
+
+          difDrive.arcadeDrive(0.2, -0.5);
+        }
+      }
+      
+
+    } else {
+      double turn = 0.5;
+      double throttle = 0.3;
+      difDrive.arcadeDrive(throttle,turn);
+    }
+  }
 
   public void setMotorsCoast() {
     // leftFrontVictor.setNeutralMode(NeutralMode.Coast);
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -134,7 +134,10 @@ public class Robot extends TimedRobot {
     // System.out.println("Teleop Periodic!");
 
     // run this command for actual driving
-    drive.drive();
+    // drive.drive();
+
+    // this command is for testing limelight tracking
+    drive.followTarget();
 
     // controller.setRumble(RumbleType.kRightRumble, 1);
     // controller.setRumble(type, value);
